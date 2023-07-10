eab17821f8e49dd2bd01d417cb9f70e04a43d33d
terryryu <ryunuri@postech.ac.kr>
2023-07-10 15:21:38 +0900

    Add some more plugins

--- lua/terryryu/packer.lua
+++ lua/terryryu/packer.lua
@@ -51,5 +51,30 @@ return require('packer').startup(function(use)
 }
 
 use {'ojroques/nvim-osc52'}
-
+use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+}
+use {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    require("which-key").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+use {'azabiong/vim-highlighter'}
+use {
+    'ggandor/leap.nvim',
+    requires = {'tpope/vim-repeat'}
+}
+use {'simrat39/symbols-outline.nvim'}
+use {
+  "folke/todo-comments.nvim",
+  requires = { "nvim-lua/plenary.nvim" },
+}
 end)
