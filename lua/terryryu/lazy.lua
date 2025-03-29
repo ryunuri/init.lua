5d5df6e53cc0984ff84156e925b8c03f0433e918
terryryu <ryunuri@postech.ac.kr>
2025-03-29 14:22:43 +0900

    comment out chatgpt

--- lua/terryryu/lazy.lua
+++ lua/terryryu/lazy.lua
@@ -93,19 +93,19 @@ local plugins = {
     },
 
     -- GPT
-    {
-      "jackMort/ChatGPT.nvim",
-        event = "VeryLazy",
-        config = function()
-          require("chatgpt").setup()
-        end,
-        dependencies = {
-          "MunifTanjim/nui.nvim",
-          "nvim-lua/plenary.nvim",
-          "folke/trouble.nvim",
-          "nvim-telescope/telescope.nvim"
-        }
-    },
+    -- {
+    --   "jackMort/ChatGPT.nvim",
+    --     event = "VeryLazy",
+    --     config = function()
+    --       require("chatgpt").setup()
+    --     end,
+    --     dependencies = {
+    --       "MunifTanjim/nui.nvim",
+    --       "nvim-lua/plenary.nvim",
+    --       "folke/trouble.nvim",
+    --       "nvim-telescope/telescope.nvim"
+    --     }
+    -- },
 
     -- Etc.
     'ojroques/nvim-osc52',
